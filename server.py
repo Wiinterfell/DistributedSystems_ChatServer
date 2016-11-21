76b886919eb39dd5da441e8cf44865abad46b4c0
Wiinterfell <slegras.contact@gmail.com>
2016-11-21 22:31:09 +0000

    DISCO + LEAVE + structure correction

--- server.py
+++ server.py
@@ -42,7 +42,7 @@ def EchoClientThread(queue, port) :
 				else:
 					roomRef = len(chatRoomsNames)
 					chatRoomsNames.append(roomName)
-					chatRoomsClients[roomName] = []
+					chatRoomsClients[roomName] = {}
 				clientName = infos[3][13:]
 				if not (clientName in clientNames):
 					clientId = len(clientNames)
@@ -50,13 +50,55 @@ def EchoClientThread(queue, port) :
 				else:
 					clientId = clientNames.index(clientName)
 				if not (clientId in chatRoomsClients[roomName]):
-					chatRoomsClients[roomName].append(clientId)
+					chatRoomsClients[roomName][clientId] = client_socket
 				print roomName
 				print roomRef
 				print clientId
 				print clientName
-				result = "JOINED_CHATROOM: " + roomName + "\nSERVER_IP: 0\nPORT: 0\nROOM_REF: " + str(roomRef) + "JOIN_ID: " + str(clientId)
+				result = "JOINED_CHATROOM: " + roomName + "\nSERVER_IP: 0\nPORT: 0\nROOM_REF: " + str(roomRef) + "\nJOIN_ID: " + str(clientId)
 				client_socket.send(result)
+
+			elif ("LEAVE_CHATROOM" in message):
+				infos = message.split("\n")
+				roomRef = int(infos[0][16:])
+				clientId = int(infos[1][9:])
+				clientName = infos[2][13:]
+				print clientName
+				if clientName in clientNames:
+					realClientId = clientNames.index(clientName)
+				else:
+					result = "ERROR: Client not recognized"
+					client_socket.send(result)
+					break
+				if roomRef in chatRoomsNames:
+					roomName = chatRoomsNames[roomRef]
+				else:
+					result = "ERROR: Unknown channel"
+					client_socket.send(result)
+					break
+
+				if (realClientId in chatRoomsClients[roomName]):
+					del chatRoomsClients[roomName][realClientId]
+					print len(chatRoomsClients[roomName])
+				result = "LEFT_CHATROOM: " + str(roomRef) + "\nJOIN_ID: " + str(clientId)
+				client_socket.send(result)
+
+			elif ("DISCONNECT" in message):
+				infos = message.split("\n")
+				clientName = infos[2][13:]
+				if (clientName in clientNames):
+					clientId = clientNames.index(clientName)
+					for i in chatRoomsClients:
+						if  clientId in chatRoomsClients[i]:
+							del chatRoomsClients[i][clientId]
+							roomRef = chatRoomsNames.index(i)
+							result = "LEFT_CHATROOM: " + str(roomRef) + "\nJOIN_ID: " + str(clientId)
+							client_socket.send(result)
+					del clientNames[clientId]
+				else:
+					result = "ERROR: Client not recognized"
+					client_socket.send(result)
+
 			else:
 				message = message.upper().rstrip()
 				client_socket.send(message)
