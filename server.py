07929d9d520e34c25a335c28ef15418b497f6cf6
Wiinterfell <slegras.contact@gmail.com>
2016-11-22 15:52:56 +0000

    String corrections int

--- server.py
+++ server.py
@@ -62,7 +62,7 @@ def EchoClientThread(queue, port) :
 				print roomRef
 				print clientId
 				print clientName
-				result = "JOINED_CHATROOM: " + roomName + "\nSERVER_IP: " + ipServer + "\nPORT: " + portServer + "\nROOM_REF: " + str(roomRef) + "\nJOIN_ID: " + str(clientId)
+				result = "JOINED_CHATROOM: " + roomName + "\nSERVER_IP: " + str(ipServer) + "\nPORT: " + str(portServer) + "\nROOM_REF: " + str(roomRef) + "\nJOIN_ID: " + str(clientId)
 				messageToRoom(result, roomName)
 
 			elif ("LEAVE_CHATROOM" in message):
