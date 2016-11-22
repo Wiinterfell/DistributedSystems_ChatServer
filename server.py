d8e881eb1cd581a9be63f47414f56f27960e9e0b
Wiinterfell <slegras.contact@gmail.com>
2016-11-22 17:08:17 +0000

    Cleaner code

--- server.py
+++ server.py
@@ -40,7 +40,6 @@ def EchoClientThread(queue, port) :
 
 			elif ("JOIN_CHATROOM" in message):
 				infos = message.split("\n")
-				print infos
 				roomName = infos[0][15:]
 				ipServer = "46.101.193.203"
 				portServer = port
@@ -58,10 +57,6 @@ def EchoClientThread(queue, port) :
 					clientId = clientNames.keys()[clientNames.values().index(clientName)]
 				if not (clientId in chatRoomsClients[roomName]):
 					chatRoomsClients[roomName][clientId] = client_socket
-				print roomName
-				print roomRef
-				print clientId
-				print clientName
 				result = "JOINED_CHATROOM: " + roomName + "\nSERVER_IP: " + str(ipServer) + "\nPORT: " + str(portServer) + "\nROOM_REF: " + str(roomRef) + "\nJOIN_ID: " + str(clientId) + "\n"
 				chatMessage = "CHAT: " + str(roomRef) + "\nCLIENT_NAME: " + clientName + "\nMESSAGE: " + clientName + " joined Chatroom\n\n"
 				client_socket.send(result)
@@ -72,7 +67,6 @@ def EchoClientThread(queue, port) :
 				roomRef = int(infos[0][16:])
 				clientId = int(infos[1][9:])
 				clientName = infos[2][13:]
-				print clientName
 				if clientName in clientNames.values():
 					realClientId = clientNames.keys()[clientNames.values().index(clientName)]
 				else:
@@ -89,10 +83,8 @@ def EchoClientThread(queue, port) :
 				chatMessage = "CHAT: " + str(roomRef) + "\nCLIENT_NAME: " + clientName + "\nMESSAGE: " + clientName + " left Chatroom\n\n"
 				client_socket.send(result)
 				messageToRoom(chatMessage, roomName)
-				print len(chatRoomsClients[roomName])
 				if (realClientId in chatRoomsClients[roomName]):
 					del chatRoomsClients[roomName][realClientId]
-					print len(chatRoomsClients[roomName])
 				else:
 					client_socket.send(result)
 				
@@ -109,7 +101,6 @@ def EchoClientThread(queue, port) :
 							result = "LEFT_CHATROOM: " + str(i) + "\nJOIN_ID: " + str(clientId) + "\n"
 							chatMessage = "CHAT: " + str(i) + "\nCLIENT_NAME: " + clientName + "\nMESSAGE: " + clientName + " left Chatroom\n\n"
 							messageToRoom(chatMessage, roomName)
-							#client_socket.send(result)
 							del chatRoomsClients[roomName][clientId]
 							found = True
 					del clientNames[clientId]
@@ -131,7 +122,6 @@ def EchoClientThread(queue, port) :
 					client_socket.send(result)
 					break
 				if roomRef < len(chatRoomsNames):
-					print "ok"
 					roomName = chatRoomsNames[roomRef]
 				else:
 					result = "ERROR: Unknown channel\n"
@@ -147,10 +137,10 @@ def EchoClientThread(queue, port) :
 
 
 			message = client_socket.recv(4096)
-		
-		#else:
-			#client_socket.close()
-			#return
+	
+
+
+
 
 if __name__ == "__main__":
 	print "*** Creating server socket"
